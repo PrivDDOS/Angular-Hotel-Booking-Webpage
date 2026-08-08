c4e51d187a9c0a2fbd1379e9591f505432190efd
Tran Anh Cuong <trananhcuong683@gmail.com>
2026-08-08 10:55:57 +0700

    Update the Angular version to 22 and make the copy button work

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component, signal, ChangeDetectionStrategy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { ClipboardModule } from '@angular/cdk/clipboard';
@@ -8,16 +8,16 @@ import { ClipboardModule } from '@angular/cdk/clipboard';
   standalone: true,
   imports: [RouterOutlet, CommonModule, ClipboardModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  changeDetection: ChangeDetectionStrategy.Eager,
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('hotel-booking');
 
   isClick = false;
-  passwordCopy = "soleil-2026";
-
+  passwordCopy = 'soleil-2026';
 
   burgerMenu(): void {
-    this.isClick = !this.isClick
+    this.isClick = !this.isClick;
   }
 }
